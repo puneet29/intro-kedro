be83ae0c8444af56484336be017cc19ddfccbb94
Puneet Saini <puneet29saini@gmail.com>
2022-02-08 09:26:49 +0000

    Added incremental dataset

--- flight_delay_prediction/src/flight_delay_prediction/pipeline_registry.py
+++ flight_delay_prediction/src/flight_delay_prediction/pipeline_registry.py
@@ -2,7 +2,7 @@
 from typing import Dict
 
 from kedro.pipeline import Pipeline, node
-from .pipelines.nodes import partition_by_day
+from .pipelines.nodes import partition_by_day, partition_calc
 
 
 def register_pipelines() -> Dict[str, Pipeline]:
@@ -13,6 +13,20 @@ def register_pipelines() -> Dict[str, Pipeline]:
     """
     return {
         "__default__": Pipeline([
-            node(partition_by_day, "ontime_2019", "ontime_2019_by_day")
+            # Uncomment below block of code to run partition dataset
+            # node(partition_by_day,
+            #      "ontime_2019",
+            #      "ontime_2019_by_day",
+            #      name="partition"),
+            node(partition_by_day,
+                 "ontime_2019",
+                 "ontime_2019_incremental",
+                 name="incremental"),
+            node(partition_calc,
+                 "ontime_2019_incremental",
+                 "ontime_2019_calc",
+                 name="calc",
+                 # Confirms if the incremental dataset has completed successfully
+                 confirms="ontime_2019_incremental")
         ])
     }
--- flight_delay_prediction/src/flight_delay_prediction/pipelines/nodes.py
+++ flight_delay_prediction/src/flight_delay_prediction/pipelines/nodes.py
@@ -11,7 +11,23 @@ def partition_by_day(df: pd.DataFrame) -> Dict[str, pd.DataFrame]:
     parts = {}
 
     for day_of_month in df["DAY_OF_MONTH"].unique():
-        parts[f"DAY_OF_MONTH == {day_of_month}"] = df[df["DAY_OF_MONTH"]
-                                                      == day_of_month]
+        # Adding zero padding to the day of month for easy sorting of day
+        parts[f"DAY_OF_MONTH == {day_of_month:02}"] = df[df["DAY_OF_MONTH"]
+                                                         == day_of_month]
+
+    return parts
+
+
+def partition_calc(partitions: Dict[str, pd.DataFrame]) -> Dict[str, str]:
+    """
+    Calculates the length of dataframe for each partition, i.e, the number of
+    rows available in dataset for each day.
+    """
+
+    parts = {}
+
+    for part_key, df in partitions.items():
+        print(f"Calc: {part_key}")
+        parts[part_key] = str(len(df))
 
     return parts
